93665ec99ea55ebf1476bdafc3405a0008f11f1c
Ayush Desai <ayushsdesai97@gmail.com>
2022-10-11 13:24:50 +0530

    Sidebar User Profile Done
    The top part for the sidebar is done.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ const Navbar = () => {
   return (
     <div className='Navbar'>
       <span className='logo'>ChatX</span>
-      <div className="user">
-        <img src='' alt=''/>
+      <div className='user'>
+        <img src='https://pps.whatsapp.net/v/t61.24694-24/296917400_1257328201698228_387654029611050022_n.jpg?ccb=11-4&oh=01_AVxnkhd59hHijK8FTj0tN16r38dWON-aidnVTrfjyBTkvQ&oe=6354943A' alt=' ' />
         <span>Ayush</span>
         <button>Log Out</button>
       </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>
-      
+    <div className='Search'>
+      <div className="SearchForm">
+        <input type='text' placeholder='Search'/>
+      </div>
+      <div className="UserChat">
+      <img src='https://thispersondoesnotexist.com/image'/>
+      <div className="UserCharInfo">
+        <span>Jill</span>
+      </div>
+
+      </div>
     </div>
   )
 }
