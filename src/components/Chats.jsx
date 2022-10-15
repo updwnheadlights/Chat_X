4386f26c99fb6155fa10f6361782b384855db313
Ayush Desai <ayushsdesai97@gmail.com>
2022-10-16 01:39:17 +0530

    Names issue solved
    Changed the names to something more professional and made the title bold.

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -4,31 +4,31 @@ const Chats = () => {
   return (
     <div className='chats'>
       <div className="UserChat">
-      <img src='https://louispeitzman.files.wordpress.com/2012/07/skyler-hazard-pay-650x364.jpg'/>
-      <div className="UserCharInfo">
-        <span>Skylar White Yo</span>
-        <p>I fucked Ted.</p>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGmr4TL9v_2oun80IJqmUAfl2DKysnL6NA0w&usqp=CAU'/>
+      <div className="UserChatInfo">
+        <span>James</span>
+        <p>Congratulations on the new Tesla!</p>
       </div>
     </div>
     <div className="UserChat">
-      <img src='https://www.cheatsheet.com/wp-content/uploads/2020/04/Mike-Ehrmantraut.jpg'/>
-      <div className="UserCharInfo">
-        <span>Mike Ehrmantraut</span>
-        <p>Here is what you are going to do to hide the body.</p>
+      <img src='https://64.media.tumblr.com/c576acc607b1d836c2b6c0e8cffe4cd4/4230a0105634184c-af/s540x810/1dafae80d6bb6f161504ade3c3241f13d00c1847.jpg'/>
+      <div className="UserChatInfo">
+        <span>Stassie</span>
+        <p>Are we still on for lunch?</p>
       </div>
     </div>
     <div className="UserChat">
-      <img src='https://mediaproxy.salon.com/width/1200/https://media.salon.com/2013/09/breaking_bad_sirota.jpg'/>
-      <div className="UserCharInfo">
-        <span>Walter White</span>
-        <p>We need to cook, NOW.</p>
+      <img src='https://t4.ftcdn.net/jpg/02/19/63/31/360_F_219633151_BW6TD8D1EA9OqZu4JgdmeJGg4JBaiAHj.jpg'/>
+      <div className="UserChatInfo">
+        <span>Sam</span>
+        <p>Can you help me with this bug?</p>
       </div>
     </div>
     <div className="UserChat">
-      <img src='https://cdn.wionews.com/sites/default/files/styles/story_page/public/2022/10/09/302415-happy-30th-birthday-2022-10-09t192101412.png'/>
-      <div className="UserCharInfo">
-        <span>It's a me Mario</span>
-        <p>Chirs Pratt fucked up my voice 😭</p>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_G9ZamQqqlzUdWqMVVkpesU543b6QUXEPQaNnzjhJ&s'/>
+      <div className="UserChatInfo">
+        <span>Kyle</span>
+        <p>Wanna play CS:GO?</p>
       </div>
     </div>
     </div>
