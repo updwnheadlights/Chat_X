be3f23f1fa15153108d2da7aadbb6f3c1136646d
Ayush Desai <ayushsdesai97@gmail.com>
2022-11-14 21:54:32 +0530

    Push
    Still fixing the chat side of things

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Img from "../img/Img.png"
+import Attach from "../img/Attach.png"
 
 const Input = () => {
   return (
     <div className='Input'>
-      Input
+      <input type="text" placeholder="Type a message" />
+      <div className="send">
+        <img src={Attach} alt='' />
+        <input type="file" style={{ display: "none" }} id="file" />
+        <label htmlFor='File'>
+          <img src={Img} alt='' />
+        </label>
+        <button>Send</button>
+      </div>
     </div>
   )
 }
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Message = () => {
     return (
-        <div>
-            Message
+        <div className='message'>
+            <div className="messageInfo">
+                <img src='https://64.media.tumblr.com/c576acc607b1d836c2b6c0e8cffe4cd4/4230a0105634184c-af/s540x810/1dafae80d6bb6f161504ade3c3241f13d00c1847.jpg' />
+                <span>Just Now</span>
+            </div>
+            <div className="messageContent">
+                <p>Hello</p>
+            </div>
         </div>
     )
 }
